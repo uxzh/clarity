15ae921fd6a71bd71aa7186fd85034956e18a50f
uxzh <77202660+uxzh@users.noreply.github.com>
2024-06-12 14:17:26 +0400

    Navbar preparation and new review design

---
+++ src/components/navbar/activeNavbar.js
@@ -0,0 +1,111 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+//import logo as svg
+import { ReactComponent as Clarity } from "../../lib/logo2.svg";
+
+export default function ActiveNavbar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Beta Registration",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar
+      onMenuOpenChange={setIsMenuOpen}
+      style={{ marginBottom: "8rem", background: "transparent" }}
+    >
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand href="/" as={Link}>
+          <Clarity style={{ width: 48, marginRight: 4 }} />
+          <p
+            style={{ color: "black" }}
+            className="font-extrabold text-black text-inherit"
+          >
+            Clarity
+          </p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-8" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Review
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Rating
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Wallet
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link style={{ color: "black" }} href="#">
+            Login
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button
+            as={Link}
+            href="https://forms.gle/kcRvqnSBm1XSQVfa7"
+            style={{ border: "1px solid #1a202c" }}
+            target="_blank"
+            variant="bordered"
+            className="shadow-[0px_3px_0px_0px_#1a202c] "
+          >
+            Beta Registration
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
--- src/pages/review.js
+++ src/pages/review.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Rating from "../components/RATING/rating";
-import Navbar from "../components/navbar/topNavbar";
+// import Rating from "../components/RATING/rating";
+import Rating from "../pages/review-v2";
+import Navbar from "../components/navbar/activeNavbar";
 
 export default function Review() {
   return (
