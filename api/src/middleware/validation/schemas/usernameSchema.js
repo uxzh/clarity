f4d26126c478da744978db404dd461aab8957e62
Lev Bulanov <levabumail@gmail.com>
2024-07-28 17:30:48 +0300

    validate username length

---
+++ api/src/middleware/validation/schemas/usernameSchema.js
@@ -0,0 +1,11 @@
+const yup = require("yup");
+
+const usernameSchema = yup.object().shape({
+  username: yup.string().required('Username is required').min(5, 'Username must be at least 5 characters').max(50, 'Username must be less than 50 characters')
+});
+
+module.exports = usernameSchema;
+
+
+
+
