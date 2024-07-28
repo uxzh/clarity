661284ac9d1095d3f15452e6d74ec150edd249f8
Lev Bulanov <levabumail@gmail.com>
2024-07-29 00:17:39 +0300

    update check username

--- api/src/controllers/usersController.js
+++ api/src/controllers/usersController.js
@@ -6,9 +6,9 @@ class UsersController {
       const { username } = req.body;
       const user = await UsersDAO.getUserByField("username", username);
       if (user && !user.error) {
-        return res.status(400).send({ error: "Username already exists" });
+        return res.status(200).send({ exists: true });
       }
-      res.status(200).send();
+      res.status(200).send({ exists: false });
     } catch (e) {
       res.status(500).send({ error: "Error checking username" });
     }
