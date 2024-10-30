7f2c632e470098ad7d3a249213cf73d2fdd97c23
Mark <77202660+uxzh@users.noreply.github.com>
2024-10-30 16:26:57 -0400

    Add "Legal Stuff" and "Privacy Policy" pages with content and styling
    * **Legal Stuff Page**
      - Create "Legal Stuff" page with content from the provided Google document
      - Add header and footer like the rest of the pages
      - Use NextUI for styling
      - Replace `Text` component with `p` tag to fix error
    
    * **Privacy Policy Page**
      - Create "Privacy Policy" page with content from the provided Google document
      - Add header and footer like the rest of the pages
      - Use NextUI for styling
      - Replace `Text` component with `p` tag to fix error

--- src/pages/privacyPolicy.js
+++ src/pages/privacyPolicy.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TopNavbar from "../components/navbar/topNavbar";
 import Footer from "../sections/Footer";
-import { Card, Text } from "@nextui-org/react";
+import { Card } from "@nextui-org/react";
 
 const PrivacyPolicy = () => {
   return (
@@ -10,51 +10,51 @@ const PrivacyPolicy = () => {
       <div className="container mx-auto p-4">
         <Card>
           <Card.Body>
-            <Text h1>Privacy Policy</Text>
-            <Text>Last Updated: 08/20/2024</Text>
-            <Text h2>1. Introduction</Text>
-            <Text>
+            <h1>Privacy Policy</h1>
+            <p>Last Updated: 08/20/2024</p>
+            <h2>1. Introduction</h2>
+            <p>
               CardsClarity, Inc. ("CardsClarity," "we," "us," or "our") operates the website located at cardsclarity.com and related services (collectively, the "Platform"). This Terms of Service and Privacy Policy ("Agreement") governs your access to and use of the Platform.
-            </Text>
-            <Text h2>2. Acceptance of Terms</Text>
-            <Text>
+            </p>
+            <h2>2. Acceptance of Terms</h2>
+            <p>
               By accessing or using the Platform, you ("User," "you," or "your") agree to be bound by this Agreement. If you do not agree to these terms, you must not access or use the Platform.
-            </Text>
-            <Text h2>3. Age Restrictions</Text>
-            <Text>
+            </p>
+            <h2>3. Age Restrictions</h2>
+            <p>
               You must be at least 13 years old to use the Platform. If you are under 18 years old, you must have parental consent to use the Platform. We do not knowingly collect or solicit personal information from anyone under 13 or knowingly allow such persons to register for the Platform. If we become aware that we have collected personal information from a child under 13 without verification of parental consent, we will take steps to remove that information from our servers.
-            </Text>
-            <Text h2>4. User-Generated Content</Text>
-            <Text h3>4.1. User Content</Text>
-            <Text>
+            </p>
+            <h2>4. User-Generated Content</h2>
+            <h3>4.1. User Content</h3>
+            <p>
               The Platform allows Users to submit, post, and share content related to credit card products, including reviews, comments, and ratings ("User Content").
-            </Text>
-            <Text h3>4.2. Ownership and License</Text>
-            <Text>
+            </p>
+            <h3>4.2. Ownership and License</h3>
+            <p>
               You retain all ownership rights to your User Content. However, by submitting User Content to the Platform, you grant CardsClarity a worldwide, non-exclusive, royalty-free, sublicensable, and transferable license to use, reproduce, distribute, prepare derivative works of, display, and perform the User Content in connection with the Platform and CardsClarity's business operations.
-            </Text>
-            <Text h3>4.3. Representations and Warranties</Text>
-            <Text>
+            </p>
+            <h3>4.3. Representations and Warranties</h3>
+            <p>
               You represent and warrant that: (i) you own the User Content posted by you on or through the Platform or otherwise have the right to grant the license set forth in this section, and (ii) your User Content does not violate the privacy rights, publicity rights, copyrights, contract rights, intellectual property rights or any other rights of any person or entity.
-            </Text>
-            <Text h3>4.4. Prohibited Content</Text>
-            <Text>
+            </p>
+            <h3>4.4. Prohibited Content</h3>
+            <p>
               You agree not to post User Content that:
-            </Text>
+            </p>
             <ul>
               <li>Is illegal, obscene, defamatory, threatening, intimidating, harassing, hateful, racially, or ethnically offensive, or instigates or encourages conduct that would be illegal or otherwise inappropriate.</li>
               <li>Infringes any patent, trademark, trade secret, copyright, or other intellectual or proprietary right of any party.</li>
               <li>Contains software viruses or any other computer code designed to interrupt, destroy, or limit the functionality of any computer software or hardware.</li>
               <li>Impersonates any person or entity or otherwise misrepresents your affiliation with a person or entity.</li>
             </ul>
-            <Text h3>4.5. Monitoring and Removal</Text>
-            <Text>
+            <h3>4.5. Monitoring and Removal</h3>
+            <p>
               CardsClarity reserves the right, but is not obligated, to monitor, edit, or remove any User Content at our sole discretion, including content that we determine violates this Agreement or may be offensive, illegal, or violate the rights of any third party.
-            </Text>
-            <Text h3>4.6. Consequences of Posting Prohibited Content</Text>
-            <Text>
+            </p>
+            <h3>4.6. Consequences of Posting Prohibited Content</h3>
+            <p>
               If you post prohibited content, CardsClarity may, at its sole discretion:
-            </Text>
+            </p>
             <ul>
               <li>Remove the content</li>
               <li>Issue a warning</li>
@@ -62,32 +62,32 @@ const PrivacyPolicy = () => {
               <li>Ban you from the Platform</li>
               <li>Take legal action if necessary</li>
             </ul>
-            <Text h2>5. Intellectual Property Rights</Text>
-            <Text h3>5.1. CardsClarity's Intellectual Property</Text>
-            <Text>
+            <h2>5. Intellectual Property Rights</h2>
+            <h3>5.1. CardsClarity's Intellectual Property</h3>
+            <p>
               The Platform and its original content (excluding User Content), features, and functionality are owned by CardsClarity and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.
-            </Text>
-            <Text h3>5.2. User's Intellectual Property</Text>
-            <Text>
+            </p>
+            <h3>5.2. User's Intellectual Property</h3>
+            <p>
               You retain all rights to any content you submit, post or display on or through the Platform. By submitting, posting or displaying content on or through the Platform, you grant us a worldwide, non-exclusive, royalty-free license to use, reproduce, adapt, publish, translate and distribute it in any and all media.
-            </Text>
-            <Text h3>5.3. Copyright Infringement</Text>
-            <Text>
+            </p>
+            <h3>5.3. Copyright Infringement</h3>
+            <p>
               If you believe that any User Content violates your copyright, please contact us at cardsclarity@gmail.com with a detailed description of the alleged infringement. We will respond to notices of alleged copyright infringement that comply with applicable law and are properly provided to us.
-            </Text>
-            <Text h2>6. Privacy Policy</Text>
-            <Text h3>6.1. Data Collection</Text>
-            <Text>
+            </p>
+            <h2>6. Privacy Policy</h2>
+            <h3>6.1. Data Collection</h3>
+            <p>
               We collect personal information you provide during account creation and platform interaction, including email address, name, account password, username, comments, and votes.
-            </Text>
-            <Text h3>6.2. Data Usage</Text>
-            <Text>
+            </p>
+            <h3>6.2. Data Usage</h3>
+            <p>
               We use collected information to manage accounts, facilitate communication, personalize content, and improve our services.
-            </Text>
-            <Text h3>6.3. Data Security</Text>
-            <Text>
+            </p>
+            <h3>6.3. Data Security</h3>
+            <p>
               We implement industry-standard encryption protocols and security measures to protect user data from unauthorized access and potential breaches. These measures include:
-            </Text>
+            </p>
             <ul>
               <li>Secure Socket Layer (SSL) technology</li>
               <li>Regular security audits</li>
@@ -95,61 +95,61 @@ const PrivacyPolicy = () => {
               <li>Limited access to personal information on a need-to-know basis</li>
               <li>Firewalls and intrusion detection systems</li>
             </ul>
-            <Text h3>6.4. Data Aggregation</Text>
-            <Text>
+            <h3>6.4. Data Aggregation</h3>
+            <p>
               Credit card-related information displayed on the Platform is collected through secure API calls and web scraping techniques from official sources. CardsClarity does not independently verify this information and makes no representations or warranties regarding its accuracy.
-            </Text>
-            <Text h3>6.5. Cookies</Text>
-            <Text>
+            </p>
+            <h3>6.5. Cookies</h3>
+            <p>
               We use cookies strictly for authorization purposes. No other data is collected or used from cookies.
-            </Text>
-            <Text h3>6.6. Third-Party Services</Text>
-            <Text>
+            </p>
+            <h3>6.6. Third-Party Services</h3>
+            <p>
               We use the following third-party services for data processing and analytics:
-            </Text>
+            </p>
             <ul>
               <li>Google Analytics for website traffic analysis</li>
               <li>Stripe for payment processing</li>
               <li>SendGrid for email communications</li>
             </ul>
-            <Text>
+            <p>
               These services may collect and process personal information as per their own privacy policies.
-            </Text>
-            <Text h3>6.7. Data Retention</Text>
-            <Text>
+            </p>
+            <h3>6.7. Data Retention</h3>
+            <p>
               We retain user data for as long as an account is active or as needed to provide services. If you delete your account, we will delete your personal information within 30 days, except for information we are required to retain for legal or business purposes.
-            </Text>
-            <Text h3>6.8. Updates to Privacy Policy</Text>
-            <Text>
+            </p>
+            <h3>6.8. Updates to Privacy Policy</h3>
+            <p>
               We will notify users of any material changes to this Privacy Policy via email and/or a prominent notice on our Platform at least 30 days prior to the changes becoming effective.
-            </Text>
-            <Text h2>7. California Consumer Privacy Act (CCPA) Rights</Text>
-            <Text>
+            </p>
+            <h2>7. California Consumer Privacy Act (CCPA) Rights</h2>
+            <p>
               For California residents, in addition to the rights described elsewhere in this Agreement, you have the following rights under the CCPA:
-            </Text>
-            <Text h3>7.1. Right to Know</Text>
-            <Text>
+            </p>
+            <h3>7.1. Right to Know</h3>
+            <p>
               You have the right to request that we disclose certain information to you about our collection and use of your personal information over the past 12 months.
-            </Text>
-            <Text h3>7.2. Right to Delete</Text>
-            <Text>
+            </p>
+            <h3>7.2. Right to Delete</h3>
+            <p>
               You have the right to request that we delete any of your personal information that we collected from you and retained, subject to certain exceptions.
-            </Text>
-            <Text h3>7.3. Right to Opt-Out of Sale</Text>
-            <Text>
+            </p>
+            <h3>7.3. Right to Opt-Out of Sale</h3>
+            <p>
               We do not sell personal information. However, if we did, you would have the right to opt-out of the sale of your personal information.
-            </Text>
-            <Text h3>7.4. Right to Non-Discrimination</Text>
-            <Text>
+            </p>
+            <h3>7.4. Right to Non-Discrimination</h3>
+            <p>
               We will not discriminate against you for exercising any of your CCPA rights.
-            </Text>
-            <Text>
+            </p>
+            <p>
               To exercise these rights, please contact us at cardsclarity@gmail.com.
-            </Text>
-            <Text h2>8. Data Breach Notification</Text>
-            <Text>
+            </p>
+            <h2>8. Data Breach Notification</h2>
+            <p>
               In the event of a data breach that compromises your personal information, we will notify you via email within 72 hours of discovering the breach. The notification will include:
-            </Text>
+            </p>
             <ul>
               <li>A description of the nature of the breach</li>
               <li>The types of information potentially involved</li>
@@ -157,67 +157,67 @@ const PrivacyPolicy = () => {
               <li>Recommended steps you can take to protect yourself</li>
               <li>Contact information for further questions or concerns</li>
             </ul>
-            <Text h2>9. Dispute Resolution and Arbitration</Text>
-            <Text h3>9.1. Informal Resolution</Text>
-            <Text>
+            <h2>9. Dispute Resolution and Arbitration</h2>
+            <h3>9.1. Informal Resolution</h3>
+            <p>
               Before initiating any arbitration or court proceeding, you agree to first contact us at cardsclarity@gmail.com and attempt to resolve any dispute informally.
-            </Text>
-            <Text h3>9.2. Arbitration Agreement</Text>
-            <Text>
+            </p>
+            <h3>9.2. Arbitration Agreement</h3>
+            <p>
               If informal resolution is unsuccessful, any dispute, controversy, or claim arising out of or relating to this Agreement, or the breach thereof, shall be determined by arbitration administered by the American Arbitration Association in accordance with its Commercial Arbitration Rules.
-            </Text>
-            <Text h3>9.3. Arbitration Process</Text>
-            <Text>
+            </p>
+            <h3>9.3. Arbitration Process</h3>
+            <p>
               The arbitration will be conducted in the English language in United States, New York. One arbitrator will be selected in accordance with the American Arbitration Association Rules. Each party will be responsible for paying any filing, administrative, and arbitrator fees in accordance with American Arbitration Association Rules.
-            </Text>
-            <Text h3>9.4. Class Action Waiver</Text>
-            <Text>
+            </p>
+            <h3>9.4. Class Action Waiver</h3>
+            <p>
               You agree that any arbitration or proceeding shall be limited to the dispute between us and you individually. To the fullest extent permitted by law, (i) no arbitration or proceeding shall be joined with any other; (ii) there is no right or authority for any dispute to be arbitrated or resolved on a class action basis or to utilize class action procedures; and (iii) there is no right or authority for any dispute to be brought in a purported representative capacity on behalf of the general public or any other persons.
-            </Text>
-            <Text h3>9.5. Opt-Out</Text>
-            <Text>
+            </p>
+            <h3>9.5. Opt-Out</h3>
+            <p>
               You have the right to opt-out and not be bound by the arbitration and class action waiver provisions by sending written notice of your decision to opt-out to cardsclarity@gmail.com within 30 days of your first use of the Platform.
-            </Text>
-            <Text h2>10. Termination</Text>
-            <Text h3>10.1. Termination by CardsClarity</Text>
-            <Text>
+            </p>
+            <h2>10. Termination</h2>
+            <h3>10.1. Termination by CardsClarity</h3>
+            <p>
               We may terminate or suspend your account and bar access to the Platform immediately, without prior notice or liability, under our sole discretion, for any reason whatsoever and without limitation, including but not limited to a breach of this Agreement.
-            </Text>
-            <Text h3>10.2. Termination by User</Text>
-            <Text>
+            </p>
+            <h3>10.2. Termination by User</h3>
+            <p>
               You may terminate your account at any time by contacting us at cardsclarity@gmail.com.
-            </Text>
-            <Text h3>10.3. Effect of Termination</Text>
-            <Text>
+            </p>
+            <h3>10.3. Effect of Termination</h3>
+            <p>
               Upon termination, your right to use the Platform will immediately cease. If you wish to terminate your account, you may simply discontinue using the Platform.
-            </Text>
-            <Text h3>10.4. Survival of Terms</Text>
-            <Text>
+            </p>
+            <h3>10.4. Survival of Terms</h3>
+            <p>
               All provisions of this Agreement which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.
-            </Text>
-            <Text h2>11. Changes to this Agreement</Text>
-            <Text>
+            </p>
+            <h2>11. Changes to this Agreement</h2>
+            <p>
               We reserve the right to modify this Agreement at any time. We will notify users of any material changes by posting the new Agreement on the Platform and updating the "Last Updated" date at the top of this Agreement. Your continued use of the Platform after any such changes constitutes your acceptance of the new Agreement.
-            </Text>
-            <Text h2>12. Contact Information</Text>
-            <Text>
+            </p>
+            <h2>12. Contact Information</h2>
+            <p>
               If you have any questions about this Agreement, please contact us at:
-            </Text>
-            <Text>
+            </p>
+            <p>
               CardsClarity, Inc.
-            </Text>
-            <Text>
+            </p>
+            <p>
               Email: cardsclarity@gmail.com
-            </Text>
-            <Text>
+            </p>
+            <p>
               Phone: +1 (650) 4540709 - Dylan Bardsley (CEO)
-            </Text>
-            <Text>
+            </p>
+            <p>
               Phone: +1 (347) 4706644 - Mark Leaf (CTO)
-            </Text>
-            <Text>
+            </p>
+            <p>
               By using the CardsClarity Platform, you acknowledge that you have read, understood, and agree to be bound by this Agreement.
-            </Text>
+            </p>
           </Card.Body>
         </Card>
       </div>
