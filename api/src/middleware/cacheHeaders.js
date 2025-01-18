4aa982c6128893823c76669936e8cde2cf4fd2b0
Zu9zwan9 <46656676+Zu9zwan9@users.noreply.github.com>
2025-01-17 23:06:45 -0400

    implemented toggle the “Write a Review” button state

--- api/src/middleware/cacheHeaders.js
+++ api/src/middleware/cacheHeaders.js
@@ -2,7 +2,7 @@ const setCacheHeaders = (seconds) => (req, res, next) => {
   if (typeof seconds !== 'number') {
     return next(new Error('Seconds must be a number'));
   }
-  res.set('Cache-Control', `max-age=0, s-maxage=${seconds}`);
+  res.set('Cache-Control', `max-age=0, s-max-age=${seconds}`);
   next();
 };
 
