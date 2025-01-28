84bfe05244786afe702da34f3ba12c0f0d537034
Tec94 <63695678+Tec94@users.noreply.github.com>
2025-01-28 17:53:34 -0600

    add secondaryNavbar component

---
+++ src/components/navbar/secondaryNavbar.js
@@ -0,0 +1,29 @@
+import {
+    Navbar,
+    NavbarContent,
+    NavbarItem,
+    Link,
+} from "@nextui-org/react";
+
+const SecondaryNavbar = () => {
+    return (
+        <Navbar shouldHideOnScroll fixed="top" shadow="none" color="light" style={{ translateX: "-13.5%", translateY: "-50%" }}>
+            <NavbarContent  className="hidden sm:flex gap-8" justify="start">
+                <NavbarItem>
+                    <Link color="foreground" href="/overview">Overview</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="/privacy-policy">Privacy Policy</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="/tos">Terms of Service</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="/faq">FAQ</Link>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    );
+}
+
+export default SecondaryNavbar;
